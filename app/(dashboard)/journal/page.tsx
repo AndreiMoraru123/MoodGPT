786102a58c08c3e60e53870767a2cfa246b4fe72
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-10-21 19:14:27 +0300

    journal page

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,4 +1,23 @@
-const JournalPage = () => {
+import { getUserByClerkId } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+const getEntries = async () => {
+    const user = await getUserByClerkId()
+    const entries = prisma.journalEntry.findMany({
+        where: {
+            userId: user.id,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+    })
+
+    return entries
+}
+
+const JournalPage = async () => {
+    const entries = await getEntries()
+    console.log(entries)
     return <div>journal</div>
 }
 
