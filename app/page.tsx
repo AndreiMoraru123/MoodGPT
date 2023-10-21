8f01bac86dbd9cd8c9aeb564af72d4b45a054160
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-10-21 12:58:08 +0300

    redirect new user to journal

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link'
+import { auth } from '@clerk/nextjs'
 
-export default function Home() {
+export default async function Home() {
+
+  const { userId } = await auth()
+  let href = userId? '/journal' : '/new-user' 
+  
   return <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
     <div className="w-full max-w-[600px] mx-auto">
         <h1 className="text-6xl mb-4">Mood GPT</h1>
         <p className="text-2xl text-white/60 mb-4">Discover Yourself</p>
         <div>
-         <Link href="/journal">
+         <Link href={href}>
             <button className="bg-red-600 p-2 rounded-lg text-xl">get started</button>
          </Link>
         </div>
