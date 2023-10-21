f7f8b0569f2b22f4c70a621628b1e15c38d39066
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-10-21 14:34:27 +0300

    hehe user button

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import { UserButton } from "@clerk/nextjs"
+
 const DashboardLayout = ({ children }) => {
     return <div className="h-screen w-screen relative">
         <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
@@ -5,7 +7,9 @@ const DashboardLayout = ({ children }) => {
         </aside>
         <div className="ml-[200px]">
             <header className="h-[60px] border-b border-black/10">
-            hello
+                <div className="h-full w-full px-6 flex items-center justify-end">
+                    <UserButton/>
+                </div>
             </header>
             <div>{children}</div>
         </div>
