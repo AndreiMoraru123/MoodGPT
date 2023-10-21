c919217dc6cf8fa753603697c54676561c2bf256
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-10-21 20:07:01 +0300

    journal UI

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,3 +1,5 @@
+import EntryCard from "@/components/EntryCard"
+import NewEntryCard from "@/components/NewEntryCard"
 import { getUserByClerkId } from "@/utils/auth"
 import { prisma } from "@/utils/db"
 
@@ -17,8 +19,20 @@ const getEntries = async () => {
 
 const JournalPage = async () => {
     const entries = await getEntries()
-    console.log(entries)
-    return <div>journal</div>
+
+    return (
+        <div className ="p-10 bg-zinc-400/10 h-full">
+            <h2 className="text-3xl mb-8">Journal</h2>
+             <div className="grid grid-cols-3 gap-4 p-10">
+                <NewEntryCard />
+                {
+                    entries.map(
+                        entry => <EntryCard key = {entry.id} entry={entry} />
+                    )
+                }
+            </div>
+        </div>
+    )
 }
 
 export default JournalPage
