8e2c1fc25b79f14752c598064a19bc3ff865c383
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-10-22 19:37:34 +0300

    analysis ui

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -16,9 +16,34 @@ const getEntry = async id => {
 
 const EntryPage = async ({ params }) => {
     const entry = await getEntry(params.id)
+    const analysisData = [
+        { name: 'Summary', value: '' },
+        { name: 'Subject', value: '' },
+        { name: 'Mood', value: '' },
+        { name: 'Negative', value: 'False' },
+    ]
     return (
-        <div className='h-full'>
-            <Editor entry={entry} />
+        <div className="h-full grid grid-cols-3">
+            <div className="col-span-2">
+                <Editor entry={entry} />
+            </div>
+            <div>
+                <div className='bg-red-300 px-5 py-5'>
+                    <h2 className="text-2xl">Analysis</h2>
+                </div>
+                <div>
+                    <ul>
+                        {analysisData.map((item) => (
+                            <li key={item.name}
+                                className='px-2  py-4 flex items-center justify-between
+                                border-b border-black/15'>
+                                <span className='text-lg font-semibold'>{item.name}</span>
+                                <span>{item.value}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
