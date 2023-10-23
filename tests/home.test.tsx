83679a9a8464615b8219a1f2592e8c8e6d7fc08c
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-10-23 17:23:18 +0300

    adds testing

---
+++ tests/home.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react'
+import { vi } from 'vitest'
+import Page from '../app/page'
+
+vi.mock('@clerk/nextjs', () => {
+  const mockedFunctions = {
+    auth: () =>
+      new Promise((resolve) =>
+        resolve({ userId: 'user_2NNEqL2nrIRdJ194ndJqAHwEfxC' })
+      ),
+    ClerkProvider: ({ children }) => <div>{children}</div>,
+    useUser: () => ({
+      isSignedIn: true,
+      user: {
+        id: 'user_2NNEqL2nrIRdJ194ndJqAHwEfxC',
+        fullName: 'Charles Harris',
+      },
+    }),
+  }
+
+  return mockedFunctions
+})
+
+vi.mock('next/font/google', () => {
+    return {
+        Inter: () => ({ className: 'inter '}),
+    }
+})
+
+test (`Home`, async () => {
+    render (await Page())
+    expect(screen.getByText('Discover Yourself')).toBeTruthy()
+})
