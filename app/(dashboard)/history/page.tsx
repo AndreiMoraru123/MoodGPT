334b6ad24f3cc61973b09d014a468613be9d3fe0
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-10-23 16:51:29 +0300

    history chart

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,3 +1,4 @@
+import HistoryChart from "@/components/HistoryChart"
 import { getUserByClerkId } from "@/utils/auth"
 import { prisma } from "@/utils/db"
 
@@ -7,20 +8,25 @@ const getData = async () => {
         where: {
             userId: user.id,
         },
-        select: {
-            sentimentScore: true,
-        },
     })
 
     const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
     const avg = Math.round(sum / analyses.length)
-    return {analyses, avg}
+    return { analyses, avg }
 }
 
 const History = async () => {
-    const {avg, analyses } = await getData()
-    console.log(analyses)
-    return <div>history: {avg}</div>
+    const { avg, analyses } = await getData()
+    return (
+        <div className="h-full px-6 py-8">
+            <div>
+                <h1 className="text-2xl mb-4">{`Avg. Sentiment: ${avg}`}</h1>
+            </div>
+            <div className="h-full w-full">
+                <HistoryChart data={analyses} />
+            </div>
+        </div>
+    )
 }
 
 export default History
