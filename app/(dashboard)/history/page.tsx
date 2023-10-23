5d7ea8de504a5917c42bbed0c3ee2f8c26a379cb
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-10-23 16:29:56 +0300

    history score

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -5,13 +5,22 @@ const getData = async () => {
     const user = await getUserByClerkId()
     const analyses = await prisma.analysis.findMany({
         where: {
-
-        }
+            userId: user.id,
+        },
+        select: {
+            sentimentScore: true,
+        },
     })
+
+    const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
+    const avg = Math.round(sum / analyses.length)
+    return {analyses, avg}
 }
 
-const History = () => {
-    return <div>history</div>
+const History = async () => {
+    const {avg, analyses } = await getData()
+    console.log(analyses)
+    return <div>history: {avg}</div>
 }
 
 export default History
